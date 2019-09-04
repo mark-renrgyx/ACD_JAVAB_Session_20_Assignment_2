ff21ab375a4c1c8df57cdff7fadfeb4484868964
Mark Reed <mark.reed@renrgyx.com>
2019-09-04 15:42:47 -0400

    code

---
+++ HibernateIntro/src/helloworld/Message.java
@@ -0,0 +1,27 @@
+package helloworld;
+
+public class Message {
+	
+	private String messageText;
+	private int id;
+	
+	public Message() {
+	}
+	
+	public String getMessageText() {
+		return messageText;
+	}
+	
+	public void setMessageText(String messageText) {
+		this.messageText = messageText;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
